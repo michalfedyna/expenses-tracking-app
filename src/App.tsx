128054d1c910bde72a697ad7df34a5b93c92e4fe
michalfedyna <michal@fedyna.pl>
2024-11-05 17:38:36 +0100

    feat: setup redux with toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { StyleSheet, Text, View } from "react-native";
+import { Provider as StoreProvider } from "react-redux";
 import { StatusBar } from "expo-status-bar";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { store } from "./store";
 
 export default function App() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
-    </GestureHandlerRootView>
+    <StoreProvider store={store}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <View style={styles.container}>
+          <Text>Open up App.tsx to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </View>
+      </GestureHandlerRootView>
+    </StoreProvider>
   );
 }
 
